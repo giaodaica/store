418f6e1c4ea7b32a0d0eb67e714d7e34cc3297b5
giaodaica <trunghieu042000@gmail.com>
2025-03-26 00:31:42 +0700

    commit gg analytic

--- resources/views/blocks/head.blade.php
+++ resources/views/blocks/head.blade.php
@@ -26,4 +26,13 @@
     <!-- custom Css-->
     <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- App js -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJBYHLG00K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LJBYHLG00K');
+</script>
 </head>
